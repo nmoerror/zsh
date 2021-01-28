71e7a27eaedc602e30d60b212d06d1cdde479d27
Alejandro Gonzalez <ale@Alejandros-MacBook-Pro.local>
2021-01-28 17:04:44 +1000

    [NMO] paths are now relevant to current user for reusability and privacy

--- example.zsh
+++ example.zsh
@@ -1,23 +1,22 @@
 # Edit Profile
-alias editconf='code /Users/ale/.oh-my-zsh/custom/example.zsh'
-alias openconf='cd /Users/ale/.oh-my-zsh/custom && code .'
+alias editconf="code /Users/$(whoami)/.oh-my-zsh/custom/example.zsh"
+alias openconf="cd /Users/$(whoami)/.oh-my-zsh/custom && code ."
 
 # Cordova
-alias cpa='cordova plugin add'
-alias cpr='cordova plugin remove'
-alias cpgins='cordova plugins'  
-alias cpfa='cordova platform add'
-alias cpfr='cordova platform remove'
+alias cpa="cordova plugin add"
+alias cpr="cordova plugin remove"
+alias cpgins="cordova plugins"  
+alias cpfa="cordova platform add"
+alias cpfr="cordova platform remove"
 
 # Move paths
-alias cdG='cd /Users/ale/gevity'
-alias cdM='cd /Users/ale/gevity/Longevum.Models'
-alias cdLT='cd /Users/ale/gevity/Longevum.Target'
-alias cdLTG='cd /Users/ale/gevity/Longevum.Target.Gevity'
-alias cdMDT='cd /Users/ale/gevity/MDSaaS.Target'
-alias cdMDTools='cd /Users/ale/gevity/MDSaaS.tools'
-alias cdMDM='cd /Users/ale/gevity/MDSaaS.Models'
-
+alias cdG="cd /Users/$(whoami)/gevity"
+alias cdM="cd /Users/$(whoami)/gevity/Longevum.Models"
+alias cdLT="cd /Users/$(whoami)/gevity/Longevum.Target"
+alias cdLTG="cd /Users/$(whoami)/gevity/Longevum.Target.Gevity"
+alias cdMDT="cd /Users/$(whoami)/gevity/MDSaaS.Target"
+alias cdMDTools="cd /Users/$(whoami)/gevity/MDSaaS.tools"
+alias cdMDM="cd /Users/$(whoami)/gevity/MDSaaS.Models"
 
 function gcdp() {
   git stash
@@ -30,39 +29,39 @@ function gcd() {
 }
 
 function minifyProject() {
-  target='Longevum';
+  target="Longevum";
   here=$(pwd);
-  cd "/Users/ale/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
+  cd "/Users/$(whoami)/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
   node wmmin.js -no-interaction -target $target -parent -output-target $target;
-  cd "/Users/ale/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
+  cd "/Users/$(whoami)/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
   node wmmin.js -no-interaction -target $target -child -output-target $target;
   cd $here;
 }
 
 function Qnify() {
-  target='Longevum';
+  target="Longevum";
   here=$(pwd);
   echo $here;
   echo $target;
-  cd "/Users/ale/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
+  cd "/Users/$(whoami)/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
   node wmmin.js -no-interaction -target $target -output-target $target -no-minification;
   cd $here;
 }
 
 function Fullnify() {
   here=$(pwd);  
-  target='Longevum';
+  target="Longevum";
   compileSassGevity;
-  cd "/Users/ale/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
+  cd "/Users/$(whoami)/gevity/MDSaaS.tools/com.mdsaas.tools/wmmin";
   node wmmin.js -no-interaction -target $target -output-target $target -no-minification;
   cd $here;
 }
 
 function compileSassGevity() {
   here=$(pwd);
-  cd /Users/ale/gevity/Longevum.Target.Gevity/com.longevum.web.gevity/app/Plugin/Gevity/webroot/css
+  cd "/Users/$(whoami)/gevity/Longevum.Target.Gevity/com.longevum.web.gevity/app/Plugin/Gevity/webroot/css";
   sass ./main.scss ./gevity.css
-  cd /Users/ale/gevity/Longevum.Target/com.longevum.app/longevum/www/css
+  cd "/Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/longevum/www/css";
   sass ./app.scss ./app.css
   cd $here;
 }
@@ -94,13 +93,13 @@ function pullAll() {
 }
 
 function IFB() {
-  target=${1:?'Run with target'};
+  target=${1:?"Run with target"};
   here=$(pwd);
   echo $target;
-  cd /Users/ale/gevity/Longevum.Target/com.longevum.app/
+  cd "/Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/";
   wmapp clean -v
   wmapp init -v
-  gevbuild config /Users/ale/gevity/ ios gevity $target
+  gevbuild config /Users/$(whoami)/gevity/ ios gevity $target
   compileSassGevity
   minifyProject
   wmapp build ios
@@ -109,7 +108,7 @@ function IFB() {
 
 function IQB() {
   here=$(pwd);
-  cd /Users/ale/gevity/Longevum.Target/com.longevum.app/
+  cd "/Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/";
   compileSassGevity
   Qnify
   wmapp build ios
@@ -117,28 +116,28 @@ function IQB() {
 }
 
 function AFB() {
-  target=${1:?'Run with target'};
+  target=${1:?"Run with target"};
   here=$(pwd);
 
-  cd /Users/ale/gevity/Longevum.Target/com.longevum.app/
+  cd "/Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/";
   wmapp clean -v
   wmapp init -v
-  gevbuild config /Users/ale/gevity/ android gevity $target
+  gevbuild config /Users/$(whoami)/gevity/ android gevity $target
   compileSassGevity
   minifyProject
   wmapp build android
   adb devices
   cd $here;
-  Write-Host 'Build to Device: adb install "/Users/ale/gevity/Longevum.Target/com.longevum.app/longevum/platforms/android/app/build/outputs/apk/debug/app-debug.apk"'
-  adb install "/Users/ale/gevity/Longevum.Target/com.longevum.app/longevum/platforms/android/app/build/outputs/apk/debug/app-debug.apk"
+  Write-Host "Build to Device: adb install /Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/longevum/platforms/android/app/build/outputs/apk/debug/app-debug.apk";
+  adb install "/Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/longevum/platforms/android/app/build/outputs/apk/debug/app-debug.apk";
 }
 
 function AQB() {
   here=$(pwd);
-  cd /Users/ale/gevity/Longevum.Target/com.longevum.app/
+  cd "/Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/";
   compileSassGevity
   Qnify
   wmapp build android
   cd $here;
-  adb install "/Users/ale/gevity/Longevum.Target/com.longevum.app/longevum/platforms/android/app/build/outputs/apk/debug/app-debug.apk"
+  adb install "/Users/$(whoami)/gevity/Longevum.Target/com.longevum.app/longevum/platforms/android/app/build/outputs/apk/debug/app-debug.apk"
 }
